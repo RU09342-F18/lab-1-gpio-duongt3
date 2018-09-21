adbd581ae28ab670b194508dfd1e35f250395e16
duongt3 <36242637+duongt3@users.noreply.github.com>
2018-09-21 15:08:22 -0400

    Update main.c

--- Button Blink/MSP430FR2311_ButtonBlink/main.c
+++ Button Blink/MSP430FR2311_ButtonBlink/main.c
@@ -11,13 +11,17 @@
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
+
 	P1SEL0 = 0;         //Set P1.0 to 0
 	P1SEL1 = 0;         //Set P1.1 to 0
+	P1OUT &= ~BIT0;     //
     P1DIR |= 0x01;      //Set P1.0 as an output
     P1DIR &= ~BIT1;     //Set P1.1 as an input
     P1REN |= BIT1;      //Set the pullup resistor on P1.1
     P1OUT |= BIT1;      //Set the output for P1.1
+
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
+                                            // to activate previously configured port settings
 
     int j;
 
@@ -26,7 +30,6 @@ int main(void)
         if(j != 2)
         {
             P1OUT ^= 0x01;
-            __delay_cycles(1000000);
         }
         else
         {
@@ -34,3 +37,4 @@ int main(void)
         }
     }
 }
+
